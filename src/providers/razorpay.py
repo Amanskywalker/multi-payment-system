91ec198f8440aa8a5efa63e499457a06f6fa08ed
aman <aman@acviss.com>
2022-10-26 17:27:56 +0530

    added basic calls

--- src/providers/razorpay.py
+++ src/providers/razorpay.py
@@ -2,6 +2,10 @@
 Razorpay Service Base configurations to be used with other services
 """
 import base64
+import json
+import requests
+
+BASE_URL = f"https://api.razorpay.com/v1"
 
 class RazorpayServiceBase:
     """
@@ -25,7 +29,52 @@ class RazorpayServiceBase:
         # generate the keys
         auth_token = base64.b64encode(f"{api_key}:{api_secret}".encode("ascii")).decode("ascii")
 
+        # attached the keys to the header
         self.HEADER['Authorization'] = f"Basic {auth_token}"
 
 
+    def excute_call(self, type='get'):
+        if type == 'get':
+            response = requests.get(self.URL, headers=self.HEADER)
+        elif type == 'post':
+            response = requests.post(self.URL, data=self.PAYLOAD, headers=self.HEADER)
+        
+        return response
+
+
+class RazorpayPayout (RazorpayServiceBase):
+    "Class to manage the payouts in razorpay X"
+
+    def create_contact(self, payload):
+        """
+        Creates the contact
+        """
+        self.PAYLOAD = json.dumps(payload)
+        self.URL = f"{BASE_URL}/contacts"
+
+        response = self.excute_call(type='post')
+
+        return response.json()
+
+    def create_fund_account(self, payload):
+        """
+        Creates the contact fund account
+        """
+        self.PAYLOAD = json.dumps(payload)
+        self.URL = f"{BASE_URL}/fund_accounts"
+
+        response = self.excute_call(type='post')
+
+        return response.json()
+
+    def create_payout(self, payload):
+        """
+        Creates the payout for the corresponding fund account
+        """
+        self.PAYLOAD = json.dumps(payload)
+        self.URL = f"{BASE_URL}/payouts"
+
+        response = self.excute_call(type='post')
+
+        return response.json()
         
